4817ab440628ba482f3aa6a949afd18f6e78ff81
Alex King <alex@askingtech.com>
2020-08-20 17:26:39 +0000

    Base theme update + ADSBI subtheme creation and setup

---
+++ web/themes/custom/bootstrap_adsbi/js/global.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function ($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.bootstrap_adsbi = {
+    attach: function (context, settings) {
+
+    }
+  };
+
+})(jQuery, Drupal);
